1db75ae72a28dec8ad26e2e80b40f0c9a3ced56d
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2022-07-15 21:07:03 +0200

    ostateczne

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let text1 = "Uwielbiam JavaScript";
+let text2 = "Jestem świetnym programistą";
+
+function dlugosc(parametr1, parametr2) {
+    
+    let wynik;
+    
+    let zmienna1 = text1.length;
+    let zmienna2 = text2.length;
+
+    if (zmienna1 > zmienna2) {
+        wynik = zmienna1;
+    } else {
+        wynik = zmienna2;
+    }
+
+    return wynik;
+}
+
+console.log(dlugosc(text1,text2));
